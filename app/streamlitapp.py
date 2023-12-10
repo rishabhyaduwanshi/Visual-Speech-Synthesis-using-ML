7d0e3bc28c6e736e3b7d1ca8f94133dc459e35d1
rishabhyaduwanshi <sidroy2002@gmail.com>
2023-12-10 22:50:52 +0530

    initial lol

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -20,11 +20,11 @@ st.set_page_config(layout='wide')
 
 st.title('Visual Speech Synthesis using ML') 
 # Generating a list of options or videos 
-options = os.listdir(os.path.join('.', 'data', 's1'))
+options = os.listdir(os.path.join('data', 's1'))
 selected_video = st.selectbox('Select a video', options)
 
 # Generate two columns 
-col1, col2 = st.columns(2)
+col1, col2,col3 = st.columns(3)
 
 if options: 
 
@@ -39,36 +39,48 @@ if options:
             # Rendering inside of the app
             video = open('test_video.mp4', 'rb')
         video_bytes = video.read() 
-        st.video(video_bytes)
+        st.video(video_bytes) 
+    st.info('This is all the machine learning model sees when making a prediction')
+    # st.info(selected_video.lower().split(".")[-1])
+    file_path = os.path.join('.','data','s1', selected_video)
+    if selected_video.lower().split(".")[-1] in ["mp4", "avi", "mkv", "mov", "wmv", "flv"]:
+        # video=open(file_path,'rb')
+        # cap = cv2.VideoCapture(file_path)
+        # video = []
+        # while cap.isOpened():
+        #     ret, frame = cap.read()
+        #     if not ret:
+        #         break
+        #     video.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))  # Convert BGR to RGB
+        # video = [cv2.resize(frame, (140, 46)) for frame in video]
+        # if video.shape[1] > 75:
+        #     video = video[:, :75, ...]
 
+        video=load_video(file_path)
+        video=video[:75,...]
+        if video.shape[0] < 75:
+            # Pad the frames with empty frames (assumed to be black frames)
+            empty_frames = np.zeros_like(video[0])  # Assuming frames are grayscale
+            num_empty_frames = 75 - video.shape[0]
+            video = np.pad(video, [(0, num_empty_frames), (0, 0),  (0, 0), (0, 0)], mode='constant', constant_values=0)
 
-    with col2: 
-        st.info('This is all the machine learning model sees when making a prediction')
-        file_path = os.path.join('.','data','s1', selected_video)
-        if selected_video.lower().split(".")[-1] in ["mp4", "avi", "mkv", "mov", "wmv", "flv"]:
+        # st.info(tf.shape(video))
+        #cap.release()
+    else:
+        video, annotations = load_data(tf.convert_to_tensor(file_path))
+        # st.info(tf.shape(video))
+    video=np.array(video).squeeze()
+    imageio.mimsave('animation.gif', video, fps=10)
+    st.image('animation.gif', width=400) 
 
-            video=load_video(file_path)
-            video=video[:75,...]
-            if video.shape[0] < 75:
-                # Pad the frames with empty frames (assumed to be black frames)
-                empty_frames = np.zeros_like(video[0])  # Assuming frames are grayscale
-                num_empty_frames = 75 - video.shape[0]
-                video = np.pad(video, [(0, num_empty_frames), (0, 0),  (0, 0), (0, 0)], mode='constant', constant_values=0)
+    st.info('Tokenized output from the Model')
+    model = load_model()
+    yhat = model.predict(tf.expand_dims(video, axis=0))
+    decoder = tf.keras.backend.ctc_decode(yhat, [75], greedy=True)[0][0].numpy()
+    st.text(decoder)
 
-        else:
-            video, annotations = load_data(tf.convert_to_tensor(file_path))
-        video=np.array(video).squeeze()
-        imageio.mimsave('animation.gif', video, fps=10)
-        st.image('animation.gif', width=400) 
-
-        st.info('Tokenized output from the Model')
-        model = load_model()
-        yhat = model.predict(tf.expand_dims(video, axis=0))
-        decoder = tf.keras.backend.ctc_decode(yhat, [75], greedy=True)[0][0].numpy()
-        st.text(decoder)
-
-        # Convert prediction to text
-        st.info('Decoded into words')
-        converted_prediction = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
-        st.text(converted_prediction)
+    # Convert prediction to text
+    st.info('Decoded into words')
+    converted_prediction = tf.strings.reduce_join(num_to_char(decoder)).numpy().decode('utf-8')
+    st.text(converted_prediction)
         
